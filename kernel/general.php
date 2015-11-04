355d5da2af863d4cf75b7c85d3f069abe442da8b
alejes <alejes@yandex.ru>
2015-11-04 22:57:40 +0300

    Add htmlpurifier for beauty and simply text enter

--- kernel/general.php
+++ kernel/general.php
@@ -1,7 +1,15 @@
 <?php
 
+include ROOT . 'libraries/htmlpurifier/library/HTMLPurifier.includes.php';
+
 function escape($str){
-	return mysql_real_escape_string(htmlspecialchars($str, ENT_QUOTES));
+	$config = HTMLPurifier_Config::createDefault();
+	$config->set('Attr.AllowedClasses',array('header')); // или Attr.ForbiddenClasses имеются ввиду CSS классы
+	$config->set('AutoFormat.RemoveEmpty',true); // удаляет пустые теги, есть исключения*
+	$config->set('HTML.Doctype','HTML 4.01 Strict'); // обратите внимание как заменился тег <strike>
+	$purifier = new HTMLPurifier($config);
+	$clean_html = $purifier->purify($str);
+	return mysql_real_escape_string($clean_html);
 }
 function redirect($url){
 	header("Location: ".$url);
