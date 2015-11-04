f26eb16d2356703638ead10a56f5c5ecabae1ce8
Andrey Kravtsun <kravtsun@cvisionlab.com>
2015-11-04 11:28:25 +0300

    Ability to load videos

--- db/test.sql
+++ db/test.sql
@@ -4,10 +4,10 @@ USE auch_db;
 insert into boards values(NULL, 'f1', 'first board', 'info on the first board');
 insert into boards values(NULL, 'f2', 'second board', 'info on the second board');
 
-call CreateThread(@thread_id, @message_id11, '11thread', 1, 'author11', 'body of my 11 thread');
-call CreateThread(@thread_id, @message_id12, '12thread', 1, 'author12', 'body of my 12 thread');
+call CreateThread(@thread_id, @message_id11, '11thread', 1, 'author11', 'body of my 11 thread', NULL, NULL);
+call CreateThread(@thread_id, @message_id12, '12thread', 1, 'author12', 'body of my 12 thread', NULL, NULL);
 
-call CreateMessage(@message_id111, @message_id11, 1, 'author111', 'body of first message into first thread of first board');
-call CreateMessage(@message_id112, @message_id111, 1, 'author111', 'body of second message into first thread of first board');
+call CreateMessage(@message_id111, @message_id11, 1, 'author111', 'body of first message into first thread of first board'  , NULL, NULL);
+call CreateMessage(@message_id112, @message_id111, 1, 'author111', 'body of second message into first thread of first board', NULL, NULL);
 
 
