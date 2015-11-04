ca1cb0845fa75c01589b4c30f5b29983970f0cff
alejes <alejes@yandex.ru>
2015-11-05 00:17:58 +0300

    fx filter for latex

--- kernel/general.php
+++ kernel/general.php
@@ -4,7 +4,7 @@ include ROOT . 'libraries/htmlpurifier/library/HTMLPurifier.includes.php';
 
 function escape($str){
 	$config = HTMLPurifier_Config::createDefault();
-	$config->set('Attr.AllowedClasses',array('header')); // или Attr.ForbiddenClasses имеются ввиду CSS классы
+	$config->set('Attr.AllowedClasses',array('header', 'math-tex')); // или Attr.ForbiddenClasses имеются ввиду CSS классы
 	$config->set('AutoFormat.RemoveEmpty',true); // удаляет пустые теги, есть исключения*
 	$config->set('HTML.Doctype','HTML 4.01 Strict'); // обратите внимание как заменился тег <strike>
 	$purifier = new HTMLPurifier($config);
