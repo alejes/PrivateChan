28f17ff99db31ee0198f03f8b3f1226b7b831a55
Andrey Kravtsun <kravtsun@cvisionlab.com>
2015-11-05 20:16:22 +0300

    Minor changes in database configuration and ddos testing.

--- db/bootstrap.sql
+++ db/bootstrap.sql
@@ -1,7 +1,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-SET GLOBAL max_connections = 100;
-SET GLOBAL innodb_buffer_pool_size = 512*1024*1024;
+SET GLOBAL max_connections = 99;
 
 -- USE auch_db;
 
--- db/stress_ddos.sh
+++ db/stress_ddos.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-seq 1 1000000 | xargs -n 1 -P $1 wget -qO- 46.101.252.174 > /dev/null
+seq 1 1000000 | xargs -n 1 -P $1 wget -qO- 46.101.252.174/a > /dev/null
