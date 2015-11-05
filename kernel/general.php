f386651a8acb488b944f55aefc61080351ac6a5d
alejes <alejes@yandex.ru>
2015-11-05 13:47:06 +0300

    Add description for HtmlPurifier

--- kernel/general.php
+++ kernel/general.php
@@ -12,9 +12,9 @@ function full_escape($str){
 
 function escape($str){
     $config = HTMLPurifier_Config::createDefault();
-    $config->set('Attr.AllowedClasses',array('header', 'math-tex', 'border')); // или Attr.ForbiddenClasses имеются ввиду CSS классы
-    $config->set('AutoFormat.RemoveEmpty',true); // удаляет пустые теги, есть исключения*
-    $config->set('HTML.Doctype','HTML 4.01 Strict'); // обратите внимание как заменился тег <strike>
+    $config->set('Attr.AllowedClasses',array('header', 'math-tex', 'border')); // или Attr.ForbiddenClasses, если имеются ввиду запрещённые CSS классы
+    $config->set('AutoFormat.RemoveEmpty',true); // удаляет пустые теги, но есть исключения описанные в документации http://htmlpurifier.org/live/configdoc/plain.html#AutoFormat.RemoveEmpty
+    $config->set('HTML.Doctype','HTML 4.01 Strict'); // преобразование некоторых тегов, например <strike>, в соответствии с HTML 4.01 Strict.
     $purifier = new HTMLPurifier($config);
     $clean_html = $purifier->purify($str);
     return mysql_real_escape_string($clean_html);
