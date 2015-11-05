265f33e609c662d7e02d51e9e54602045e67667f
alejes <alejes@yandex.ru>
2015-11-05 12:32:45 +0300

    add install guide and ordered db files

--- db/1_bootstrap.sql
+++ db/1_bootstrap.sql
@@ -1,4 +1,4 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+﻿SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 -- USE auch_db;
@@ -122,6 +122,4 @@ SELECT
 FROM boards b LEFT JOIN threads t ON t.board_id = b.board_id 
    LEFT JOIN threads_message_counts tmc ON tmc.thread_id = t.thread_id
 GROUP BY board_id;
-    
-source procedures.sql;
-
+ 
